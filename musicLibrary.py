14638688270803a909bb6fc006468bf08cbecc2a
gitPriyakumari <india.priyakumari@gmail.com>
2025-07-16 17:02:22 +0530

    changes

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music ={
+    "happy":"https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "cat":"https://www.youtube.com/watch?v=u5DkAV6IXp0",
+    "dog":"https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "sun":"https://www.youtube.com/watch?v=JgDNFQ2RaLQ"
+}
